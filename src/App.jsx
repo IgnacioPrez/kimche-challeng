5c3224f0e174312b213d42c182a1e89dc41742f1
Ignacio Perez <ignacio.perez.jobs@gmail.com>
2024-03-29 16:30:55 -0300

    style(ui): :lipstick: styling in components

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Characters, Filters, SearchCharacter } from "./components";
 export default function App() {
   return (
     <>
-      <main>
+      <main className="bg-[#1a1a1a] w-screen h-screen flex pt-8 items-center flex-col gap-4">
         <SearchCharacter/>
         <Filters/>
         <Characters/>
