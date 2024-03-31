f5d8d16ee88497ecb8e44cfcbb747f70d9380fa9
Ignacio Perez <ignacio.perez.jobs@gmail.com>
2024-03-31 16:03:03 -0300

    fix(new function): :lipstick: scroll correction

--- src/components/characters/Characters.jsx
+++ src/components/characters/Characters.jsx
@@ -29,7 +29,8 @@ const Characters = ({
     getSearchMatches({ variables: { id } })
   }
   const handleInfiniteScroll = (e) => {
-    const isAtBottom = e.currentTarget.scrollHeight - e.currentTarget.scrollTop === e.currentTarget.clientHeight
+    const isAtBottom = e.currentTarget.scrollHeight - (e.currentTarget.scrollTop + e.currentTarget.clientHeight) <= 1;
+    console.log(isAtBottom)
     if (isAtBottom) {
       if (resultOfSearch.length > 0) {
         loadMoreCharacters(searchMore, dataOfSearch)
@@ -43,7 +44,7 @@ const Characters = ({
 
   return (
     <div
-      className="w-[80%] flex items-center gap-5 justify-center flex-wrap min-h-[80vh] max-h-[80vh] overflow-y-auto  md:min-h-[80vh] md:max-h-[80vh] xl:min-h-[100vh] xl:max-h-[100vh] lg:min-h-[70vh] lg:max-h-[70vh]"
+      className="w-[80%] flex items-center gap-5 justify-center flex-wrap min-h-[80vh] max-h-[80vh]  overflow-y-auto"
       onScroll={handleInfiniteScroll}
     >
       {loading || loadingSearch
--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,7 +1,6 @@
 export default function Modal({ setShowModal, character }) {
   const { name, gender, image, location, origin, species, status } = character
 
-  console.log(character)
   return (
     <>
       <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none ">
