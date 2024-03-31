cc6ebcb3dc3312244bb6cd4f682289644fe32bf9
Ignacio Perez <ignacio.perez.jobs@gmail.com>
2024-03-31 12:36:50 -0300

    fix: :adhesive_bandage: remove component and state page
    Page related code removed

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 export { default as Characters } from './characters/Characters';
 export { default as Filters } from './filters/Filters';
 export { default as Modal } from './modal/Modal';
-export { default as Pagination } from './pagination/Pagination';
 export { default as SearchCharacter } from './search-character/SearchCharacter';
 export { default as Skeleton } from './skeleton/Skeleton';
